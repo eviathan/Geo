797842e4966312b8f8b94a94929d6480d62ac051
Brian Williams <emial.brianwilliams@gmail.com>
2019-04-20 13:30:02 +0100

    More refactoring

---
+++ Geo/Geo/Views/Layers/Node.swift
@@ -0,0 +1,9 @@
+//
+//  Node.swift
+//  Geo
+//
+//  Created by Brian on 20/04/2019.
+//  Copyright © 2019 Eviathan. All rights reserved.
+//
+
+import Foundation
---
+++ Geo/Geo/Views/Layers/Polygon.swift
@@ -0,0 +1,9 @@
+//
+//  G.swift
+//  Geo
+//
+//  Created by Brian on 20/04/2019.
+//  Copyright © 2019 Eviathan. All rights reserved.
+//
+
+import Foundation
